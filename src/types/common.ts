a2b144c394020219792def4ed6ab42e9d23c0cfb
Miinkyuuu <yng1404@gmail.com>
2025-06-22 11:32:11 +0900

    :sparkles: [feat] : itemTypes 수정

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,7 +1,17 @@
 export interface ItemTypes {
+  id: number;
   title: string;
   content: string;
   image: string;
   place: string;
   date: string;
+  user: UserTypes;
+  views: number;
+  chatCount: number;
 }
+
+type UserTypes = {
+  id: number;
+  name: string;
+  profileImage: string;
+};
