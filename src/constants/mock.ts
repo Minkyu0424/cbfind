27fde9f73c4e997920613d484a09803c7a9cd400
Miinkyuuu <yng1404@gmail.com>
2025-06-22 11:17:27 +0900

    :sparkles: [feat] : 게시물 아이템 임시 목 데이터 생성

---
+++ src/constants/mock.ts
@@ -0,0 +1,74 @@
+import type { ItemTypes } from "../types/common";
+
+export const MockItems: ItemTypes[] = [
+  {
+    title: "에어팟 프로",
+    content: "학생회관 1층에서 주웠어요",
+    image: "/mockItem.png",
+    place: "학생회관",
+    date: "2025-06-18",
+  },
+  {
+    title: "아이패드",
+    content: "중도 2층 열람실에서 발견했습니다.",
+    image: "/mockipad.jpg",
+    place: "중앙도서관",
+    date: "2025-06-17",
+  },
+  {
+    title: "검정 우산",
+    content: "농생대 앞 벤치에 놓여 있었어요.",
+    image: "/mockumb.jpeg",
+    place: "농생대",
+    date: "2025-06-16",
+  },
+  {
+    title: "지갑",
+    content: "버스 정류장 근처에 떨어져 있었습니다.",
+    image: "/mockpurse.jpg",
+    place: "정문 버스정류장",
+    date: "2025-06-15",
+  },
+  {
+    title: "보조배터리",
+    content: "기계관 자판기 위에 있었어요.",
+    image: "/mockbat.jpg",
+    place: "기계관",
+    date: "2025-06-14",
+  },
+  {
+    title: "노트북",
+    content: "공대 7호관 강의실에서 주웠습니다.",
+    image: "/mock note.jpg",
+    place: "공대7호관",
+    date: "2025-06-13",
+  },
+  {
+    title: "스마트워치",
+    content: "운동장에서 발견했어요.",
+    image: "/mockwatch.jpg",
+    place: "운동장",
+    date: "2025-06-12",
+  },
+  {
+    title: "이어폰",
+    content: "학생회관 화장실 앞에 떨어져 있었습니다.",
+    image: "/mockwar.jpg",
+    place: "학생회관",
+    date: "2025-06-11",
+  },
+  {
+    title: "필통",
+    content: "도서관 그룹스터디룸에 있었습니다.",
+    image: "/mockpen.jpg",
+    place: "중앙도서관",
+    date: "2025-06-10",
+  },
+  {
+    title: "휴대폰",
+    content: "후문 근처 벤치에서 주웠어요.",
+    image: "/mockphone.jpg",
+    place: "후문",
+    date: "2025-06-09",
+  },
+];
---
+++ src/types/common.ts
@@ -0,0 +1,7 @@
+export interface ItemTypes {
+  title: string;
+  content: string;
+  image: string;
+  place: string;
+  date: string;
+}
